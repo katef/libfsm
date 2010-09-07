b364227b16d585ccee24a79e0632d6979f13e448
Kate F <kate@elide.org>
2010-09-07 14:53:13 +0000

    Added provision for duplicate labels in NFA, by creating an epsilon transition to a newly-created state to hold the duplicate edge.

--- src/libfsm/edge.c
+++ src/libfsm/edge.c
@@ -58,7 +58,31 @@ fsm_addedge_literal(struct fsm *fsm, struct fsm_state *from, struct fsm_state *t
 	assert(from != NULL);
 	assert(to != NULL);
 
-	/* TODO: provide for duplicate labels; create an epsilon transition on the fly */
+	/*
+	 * NFA may have duplicate edges. Since edges are stored in an array indexed
+	 * by c, duplicate transitions cannot be expressed directly. Instead, we
+	 * create a dummy epsilon transition which serves to hold a newly-created
+	 * state, and transition from that state instead.
+	 */
+	if (from->edges[(unsigned char) c] != NULL) {
+		struct fsm_state *new;
+
+		/*
+		 * XXX: this causes strange effects when the user doesn't expect state
+		 * numbering to change. have inventid() pick descending values from
+		 * UINT_MAX instead
+		 */
+		new = fsm_addstate(fsm, 0);
+		if (new == NULL) {
+			return 0;
+		}
+
+		if (!fsm_addedge_epsilon(fsm, from, new)) {
+			return 0;
+		}
+
+		from = new;
+	}
 
 	from->edges[(unsigned char) c] = to;
 
