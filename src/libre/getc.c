81a242aadcd0185d147dba255f7d3c513b104d4d
Kate F <kate@elide.org>
2010-09-15 14:50:27 +0000

    Oops... this should have been in [8].

---
+++ src/libre/getc.c
@@ -0,0 +1,41 @@
+/* $Id$ */
+
+#include <assert.h>
+#include <stdio.h>
+
+#include <re/re.h>
+
+int
+re_getc_str(void *opaque)
+{
+	char **s;
+	char c;
+
+	s = opaque;
+
+	assert(s  != NULL);
+	assert(*s != NULL);
+
+	c = **s;
+
+	if (c == '\0') {
+		return EOF;
+	}
+
+	(*s)++;
+
+	return c;
+}
+
+int
+re_getc_file(void *opaque)
+{
+	FILE *f;
+
+	f = opaque;
+
+	assert(f  != NULL);
+
+	return fgetc(f);
+}
+
