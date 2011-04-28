95f8834a81b6a73a5c87b943117e7dc932f84996
Kate F <kate@elide.org>
2011-04-28 18:15:35 +0000

    Added `-g`.

--- share/mk/lx.cc.mk
+++ share/mk/lx.cc.mk
@@ -25,3 +25,20 @@ CFLAGS+= -O4
 
 .endif
 
+
+.if ${CC:Mgcc} && !defined(NDEBUG)
+
+CFLAGS+= -g -ggdb
+
+.elif ${CC:Mpcc}
+
+CFLAGS:= ${CFLAGS:N-O}
+CFLAGS+= -g
+
+.elif ${CC:Mclang}
+
+CFLAGS:= ${CFLAGS:N-O*}
+CFLAGS+= -g -O0
+
+.endif
+
