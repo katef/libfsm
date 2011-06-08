66f19007ba3c9ab0b22a2135959efa855ba2be19
Kate F <kate@elide.org>
2011-06-08 08:31:54 +0000

    Reworked CLI arguments; `-d`, `-r` etc are not just conveniences on top of `-t <transformation>`. Likewise for queryies.

--- src/bin/fsm/main.c
+++ src/bin/fsm/main.c
@@ -2,6 +2,7 @@
 
 #include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
@@ -14,13 +15,14 @@
 
 #include "parser.h"
 
-#include "libfsm/internal.h"	/* XXX */
-
 extern int optind;
 extern char *optarg;
 
+FILE *out;
+
 void usage(void) {
-	printf("usage: fsm [-chagdmr] [-l <language>] [-n <prefix>] [-e <execution> | -q <query] [<input> [<output>]]\n");
+	printf("usage: fsm [-chagdpmr] [-o <file> ] [-P <file>] [-l <language>] [-n <prefix>]\n"
+	       "           [-t <transformation>] [-e <execution> | -q <query>]\n");
 }
 
 static int colour_hook_compare(const struct fsm *fsm, void *a, void *b) {
@@ -45,116 +47,174 @@ static int colour_hook_print(const struct fsm *fsm, FILE *f, void *colour) {
 	return fprintf(f, "%s", (const char *) colour);
 }
 
-static FILE *xopen(int argc, char * const argv[], int i, FILE *f, const char *mode) {
-	if (argc <= i || 0 == strcmp("-", argv[i])) {
-		return f;
+static int query(struct fsm *fsm, const char *name) {
+	size_t i;
+
+	struct {
+		const char *name;
+		int (*f)(const struct fsm *);
+	} a[] = {
+		{ "isdfa",     fsm_isdfa  },
+		{ "dfa",       fsm_isdfa  },
+		{ "hasend",    fsm_hasend },
+		{ "end",       fsm_hasend },
+		{ "accept",    fsm_hasend },
+		{ "hasaccept", fsm_hasend }
+	};
+
+	assert(fsm != NULL);
+	assert(name != NULL);
+
+	for (i = 0; i < sizeof a / sizeof *a; i++) {
+		if (0 == strcmp(a[i].name, name)) {
+			return a[i].f(fsm);
+		}
 	}
 
-	f = fopen(argv[i], mode);
-	if (f == NULL) {
-		perror(argv[i]);
-		exit(EXIT_FAILURE);
+	fprintf(stderr, "unrecognised query; valid queries are: "
+		"isdfa, hasend\n");
+	exit(EXIT_FAILURE);
+}
+
+static void transform(struct fsm *fsm, const char *name) {
+	size_t i;
+
+	struct {
+		const char *name;
+		int (*f)(struct fsm *);
+	} a[] = {
+		{ "reverse",  fsm_reverse  },
+		{ "rev",      fsm_reverse  },
+		{ "dfa",      fsm_todfa    },
+		{ "todfa",    fsm_todfa    },
+		{ "min",      fsm_minimize },
+		{ "minimize", fsm_minimize }
+	};
+
+	assert(fsm != NULL);
+	assert(name != NULL);
+
+	for (i = 0; i < sizeof a / sizeof *a; i++) {
+		if (0 == strcmp(a[i].name, name)) {
+			if (!a[i].f(fsm)) {
+				fprintf(stderr, "couldn't transform\n");
+				exit(EXIT_FAILURE);
+			}
+			return;
+		}
 	}
 
-	return f;
+	fprintf(stderr, "unrecognised transformation; valid transformations are: "
+		"reverse, todfa, minimize\n");
+	exit(EXIT_FAILURE);
+}
+
+static enum fsm_out language(const char *name) {
+	size_t i;
+
+	struct {
+		const char *name;
+		enum fsm_out format;
+	} a[] = {
+		{ "dot",   FSM_OUT_DOT   },
+		{ "table", FSM_OUT_TABLE },
+		{ "c",     FSM_OUT_C     }
+	};
+
+	assert(name != NULL);
+
+	for (i = 0; i < sizeof a / sizeof *a; i++) {
+		if (0 == strcmp(a[i].name, name)) {
+			return a[i].format;
+		}
+	}
+
+	fprintf(stderr, "unrecognised output language; valid languages are: "
+		"fsm, dot, table, c\n");
+	exit(EXIT_FAILURE);
+}
+
+static void cleanup(void) {
+	if (out != NULL) {
+		fclose(out);
+	}
 }
 
 int main(int argc, char *argv[]) {
-	FILE *in;
-	FILE *out;
 	enum fsm_out format = FSM_OUT_FSM;
 	struct fsm *fsm;
-
-	struct cli_options {
-		/* boolean: reverse the FSM as per fsm_reverse() */
-		unsigned int reverse:1;
-
-		/* boolean: convert to a DFA as per fsm_todfa() */
-		unsigned int todfa:1;
-
-		/* boolean: minimize redundant transitions */
-		unsigned int minimize:1;
-
-		/* boolean: query properties */
-		unsigned int query:1;
-		enum query { QUERY_DFA, QUERY_END } query_property;
-
-		const char *execute;
-	};
 
 	static const struct fsm_outoptions outoptions_defaults;
 	struct fsm_outoptions out_options = outoptions_defaults;
 
-	static const struct cli_options cli_options_defaults;
-	struct cli_options cli_options = cli_options_defaults;
+	{
+		static const struct fsm_colour_hooks hooks = {
+			colour_hook_compare,
+			colour_hook_print
+		};
+
+		fsm = fsm_parse(stdin);
+		if (fsm == NULL) {
+			exit(EXIT_FAILURE);
+		}
+
+		fsm_setcolourhooks(fsm, &hooks);
+	}
+
+	atexit(cleanup);
 
 	{
 		int c;
 
-		while ((c = getopt(argc, argv, "hagn:l:de:cmrq:")) != -1) {
+		while ((c = getopt(argc, argv, "hagn:l:de:o:cmrt:pP:q:")) != -1) {
 			switch (c) {
+			case 'a': out_options.anonymous_states  = 1;      break;
+			case 'g': out_options.fragment          = 1;      break;
+			case 'c': out_options.consolidate_edges = 1;      break;
+			case 'n': out_options.prefix            = optarg; break;
+
+			case 'e': return fsm_exec(fsm, fsm_sgetc, &optarg);
+
+			case 'P':
+				{
+					FILE *f;
+
+					f = 0 == strcmp(optarg, "-") ? stdout : fopen(optarg, "w");
+					if (f == NULL) {
+						perror(optarg);
+						exit(EXIT_FAILURE);
+					}
+
+					fsm_print(fsm, f, format, &out_options);
+
+					fclose(f);
+				}
+				break;
+
+			case 'p':
+				out = stdout;
+				break;
+
+			case 'o':
+				out = 0 == strcmp(optarg, "-") ? stdout : fopen(optarg, "w");
+				if (out == NULL) {
+					perror(optarg);
+					exit(EXIT_FAILURE);
+				}
+				break;
+
+			case 'l': format = language(optarg);  break;
+
+			case 'd': transform(fsm, "todfa");    break;
+			case 'm': transform(fsm, "minimize"); break;
+			case 'r': transform(fsm, "reverse");  break;
+			case 't': transform(fsm, optarg);     break;
+
+			case 'q': exit(query(fsm, optarg));   break;
+
 			case 'h':
 				usage();
 				exit(EXIT_SUCCESS);
-
-			case 'a':
-				out_options.anonymous_states = 1;
-				break;
-
-			case 'g':
-				out_options.fragment = 1;
-				break;
-
-			case 'n':
-				out_options.prefix = optarg;
-				break;
-
-			case 'l':
-				if (0 == strcmp(optarg, "fsm")) {
-					format = FSM_OUT_FSM;
-				} else if (0 == strcmp(optarg, "dot")) {
-					format = FSM_OUT_DOT;
-				} else if (0 == strcmp(optarg, "table")) {
-					format = FSM_OUT_TABLE;
-				} else if (0 == strcmp(optarg, "c")) {
-					format = FSM_OUT_C;
-				} else {
-					fprintf(stderr, "unrecognised output language; valid languages are: fsm, dot, table, c\n");
-					exit(EXIT_FAILURE);
-				}
-				break;
-
-			case 'd':
-				cli_options.todfa = 1;
-				break;
-
-			case 'e':
-				cli_options.execute = optarg;
-				break;
-
-			case 'm':
-				cli_options.minimize = 1;
-				break;
-
-			case 'r':
-				cli_options.reverse = 1;
-				break;
-
-			case 'c':
-				out_options.consolidate_edges = 1;
-				break;
-
-			case 'q':
-				cli_options.query = 1;
-				if (0 == strcmp(optarg, "dfa")) {
-					cli_options.query_property = QUERY_DFA;
-				} else if (0 == strcmp(optarg, "end")) {
-					cli_options.query_property = QUERY_END;
-				} else {
-					fprintf(stderr, "unrecognised query property; valid properties are: dfa, end\n");
-					exit(EXIT_FAILURE);
-				}
-				break;
 
 			case '?':
 			default:
@@ -163,90 +223,20 @@ int main(int argc, char *argv[]) {
 			}
 		}
 
-		if (cli_options.query && cli_options.execute != NULL) {
-			fprintf(stderr, "query and execution are mutually exclusive\n");
-			exit(EXIT_FAILURE);
-		}
-
 		argc -= optind;
 		argv += optind;
 	}
 
-	if (argc > 2) {
+	if (argc != 0) {
 		usage();
 		exit(EXIT_FAILURE);
 	}
 
-	in  = xopen(argc, argv, 0, stdin,  "r");
-	out = xopen(argc, argv, 1, stdout, "w");
-
-	{
-		static const struct fsm_colour_hooks hooks = {
-			colour_hook_compare,
-			colour_hook_print
-		};
-
-		fsm = fsm_parse(in);
-		if (fsm == NULL) {
-			exit(EXIT_FAILURE);
-		}
-
-		fsm_setcolourhooks(fsm, &hooks);
-
-		if (cli_options.reverse) {
-			if (!fsm_reverse(fsm)) {
-				perror("fsm_reverse");
-				exit(EXIT_FAILURE);
-			}
-		}
-
-		if (cli_options.todfa) {
-			if (!fsm_todfa(fsm)) {
-				perror("fsm_todfa");
-				exit(EXIT_FAILURE);
-			}
-		}
-
-		if (cli_options.minimize) {
-			if (!fsm_minimize(fsm)) {
-				perror("fsm_minimize");
-				exit(EXIT_FAILURE);
-			}
-		}
-
-		/* TODO: make optional */
+	if (out != NULL) {
 		fsm_print(fsm, out, format, &out_options);
-
-		if (cli_options.execute != NULL) {
-			int e;
-
-			e = fsm_exec(fsm, fsm_sgetc, &cli_options.execute);
-
-			fsm_free(fsm);
-
-			return e ? 0 : 1;
-		}
-
-		if (cli_options.query) {
-			int q = 0;
-
-			switch (cli_options.query_property) {
-			case QUERY_DFA:
-				q = fsm_isdfa(fsm);
-				break;
-
-			case QUERY_END:
-				q = fsm_hasend(fsm);
-				break;
-			}
-
-			fsm_free(fsm);
-
-			return q ? 0 : 1;
-		}
-
-		fsm_free(fsm);
 	}
+
+	fsm_free(fsm);
 
 	return 0;
 }
