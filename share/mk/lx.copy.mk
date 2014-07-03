0b0447c21dfabfee9ab3515168f3bfb9f8b9bb16
Kate F <kate@elide.org>
2014-07-03 15:03:13 +0000

    Added `cgi` and `xhtml5` extensions.

--- share/mk/lx.copy.mk
+++ share/mk/lx.copy.mk
@@ -15,19 +15,29 @@
 .endfor
 
 .for file in ${FILES}
+all:: ${OBJ_SDIR}/${file:T}
+.endfor
+
+.for file in ${FILES}
 ${OBJ_SDIR}/${file:T}: ${file}
 	@${MKDIR} "${OBJ_SDIR}"
 .if ${file:E} == xhtml
+	${XMLLINT} ${XMLFLAGS} --noout --dtdattr ${.ALLSRC}
+.elif ${file:E} == xhtml5
 	${XMLLINT} ${XMLFLAGS} --noout --dtdattr ${.ALLSRC}
 .elif ${file:E} == xsl
 	${XMLLINT} ${XMLFLAGS} --noout ${.ALLSRC}
 	${XSLTPROC} ${XSLTFLAGS} ${.ALLSRC}
 .endif
+.if ${file:E} == cgi
+	${COPYEXEC} ${.ALLSRC} ${.TARGET}
+.else
 	${COPYFILE} ${.ALLSRC} ${.TARGET}
+.endif
 .endfor
 
 
-CLEAN+= ${FILES:/^/${OBJ_SDIR}\//}
+CLEAN+= ${FILES:S/^/${OBJ_SDIR}\//}
 
 .include <lx.clean.mk>
 
