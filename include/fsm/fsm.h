d3beec5882f945677d47d42c35d8e05e549dab46
Kate F <kate@elide.org>
2014-07-17 18:02:55 +0000

    Added `fsm_reachable()`.

--- include/fsm/fsm.h
+++ include/fsm/fsm.h
@@ -199,5 +199,17 @@ int
 fsm_all(const struct fsm *fsm,
 	int (*predicate)(const struct fsm *, const struct fsm_state *));
 
+/*
+ * Assert that the given predicate holds for all states reachable
+ * (by epsilon transitions or otherwise) from the given state.
+ *
+ * Returns 1 if the predicate holds for all states reached, 0 otherwise.
+ *
+ * Returns -1 on error.
+ */
+int
+fsm_reachable(struct fsm *fsm, struct fsm_state *state,
+	int (*predicate)(const struct fsm *, const struct fsm_state *));
+
 #endif
 
---
+++ src/libfsm/reachable.c
@@ -0,0 +1,137 @@
+/* $Id$ */
+
+#include <assert.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+#include <adt/set.h>
+#include <adt/xalloc.h>
+
+#include <fsm/fsm.h>
+#include <fsm/pred.h>
+
+#include "internal.h"
+
+struct list {
+	struct fsm_state *state;
+	unsigned int done:1;
+	struct list *next;
+};
+
+static struct list *
+list_push(struct list **list, struct fsm_state *state)
+{
+	struct list *new;
+
+	new = malloc(sizeof *new);
+	if (new == NULL) {
+		return NULL;
+	}
+
+	new->done  = 0;
+	new->state = state;
+
+	new->next = *list;
+	*list = new;
+
+	return new;
+}
+
+static struct list *
+list_nextnotdone(struct list *list)
+{
+	struct list *p;
+
+	for (p = list; p != NULL; p = p->next) {
+		if (p->done) {
+			continue;
+		}
+
+		return p;
+	}
+
+	return NULL;
+}
+
+static int
+list_contains(const struct list *list, const struct fsm_state *state)
+{
+	const struct list *p;
+
+	for (p = list; p != NULL; p = p->next) {
+		if (p->state == state) {
+			return 1;
+		}
+	}
+
+	return 0;
+}
+
+static void
+list_free(struct list *list)
+{
+	struct list *p;
+	struct list *next;
+
+	for (p = list; p != NULL; p = next) {
+		next = p->next;
+
+		free(p);
+	}
+}
+
+int
+fsm_reachable(struct fsm *fsm, struct fsm_state *state,
+	int (*predicate)(const struct fsm *, const struct fsm_state *))
+{
+	struct list *list;
+	struct list *p;
+
+	assert(state != NULL);
+	assert(predicate != NULL);
+
+	/*
+	 * Iterative depth-first search.
+	 *
+	 * It might be more sensible to keep "visited" information in fsm_state
+	 * rather than going to the trouble of maintaining a list here. But for
+	 * the moment I want to keep such things self-contained.
+	 */
+	/* TODO: write in terms of fsm_walk or some common iteration callback */
+
+	list = NULL;
+
+	if (!list_push(&list, state)) {
+		return -1;
+	}
+
+	while (p = list_nextnotdone(list), p != NULL) {
+		struct state_set *e;
+		int i;
+
+		if (!predicate(fsm, p->state)) {
+			list_free(p);
+			return 0;
+		}
+
+		for (i = 0; i <= FSM_EDGE_MAX; i++) {
+			for (e = p->state->edges[i].sl; e != NULL; e = e->next) {
+				/* not a list operation... */
+				if (list_contains(list, e->state)) {
+					continue;
+				}
+
+				if (!list_push(&list, e->state)) {
+					return -1;
+				}
+			}
+		}
+
+		p->done = 1;
+	}
+
+	list_free(list);
+
+	return 1;
+}
+
