8bb83d62a37d82320b5f651406832b124ef46fe9
Kate F <kate@elide.org>
2014-07-23 15:55:05 +0000

    A priority queue.
    This has a few fields (and operations) which don't really belong in the ADT, but they can live here for now.

---
+++ include/adt/priq.h
@@ -0,0 +1,44 @@
+/* $Id$ */
+
+#ifndef PRIQ_H
+#define PRIQ_H
+
+struct fsm_state;
+
+struct priq {
+	struct priq *next;
+	unsigned cost;
+
+	/* XXX: specific to shortest.c */
+	struct fsm_state *state;
+	struct priq *prev; /* previous node in shortest path. we could store the edge's character instead */
+	int type; /* XXX: should really be fsm_edge_type */
+};
+
+/*
+ * Pop the minimum cost node.
+ */
+struct priq *
+priq_pop(struct priq **priq);
+
+/*
+ * Enqueue with priority.
+ */
+struct priq *
+priq_push(struct priq **priq,
+	struct fsm_state *state, unsigned int cost);
+
+/* XXX: set operation */
+struct priq *
+priq_find(struct priq *priq, struct fsm_state *state);
+
+/* XXX: set operation */
+void
+priq_move(struct priq **priq, struct priq *new);
+
+/* XXX: set operation */
+void
+priq_free(struct priq *priq);
+
+#endif
+
---
+++ src/adt/priq.c
@@ -0,0 +1,104 @@
+/* $Id$ */
+
+#include <assert.h>
+#include <stdlib.h>
+
+#include <adt/priq.h>
+
+struct priq *
+priq_pop(struct priq **priq)
+{
+	struct priq *p;
+
+	if (*priq == NULL) {
+		return NULL;
+	}
+
+	/*
+	 * The minimum cost is always the first node, since this is
+	 * a priority priq and therefore nodes are inserted in order.
+	 */
+
+	p = *priq;
+
+	*priq = p->next;
+	p->next = NULL;
+
+	return p;
+}
+
+struct priq *
+priq_push(struct priq **priq,
+	struct fsm_state *state, unsigned int cost)
+{
+	struct priq *new;
+	struct priq **p;
+
+	assert(priq != NULL);
+	assert(state != NULL);
+
+	/*
+	 * This is more expensive than it could be; I'm
+	 * opting for simplicity of implementation here.
+	 */
+
+	for (p = priq; *p != NULL; p = &(*p)->next) {
+		if ((*p)->cost >= cost) {
+			break;
+		}
+	}
+
+	new = malloc(sizeof *new);
+	if (new == NULL) {
+		return NULL;
+	}
+
+	new->cost  = cost;
+	new->state = state;
+	new->prev  = NULL;
+	new->type  = -1; /* XXX: nothing meaningful to set here */
+
+	new->next = *p;
+	*p = new;
+
+	return new;
+}
+
+struct priq *
+priq_find(struct priq *priq, struct fsm_state *state)
+{
+	struct priq *p;
+
+	assert(state != NULL);
+
+	for (p = priq; p != NULL; p = p->next) {
+		if (p->state == state) {
+			return p;
+		}
+	}
+
+	return NULL;
+}
+
+void
+priq_move(struct priq **priq, struct priq *new)
+{
+	assert(priq != NULL);
+	assert(new->next == NULL);
+
+	new->next = *priq;
+	*priq = new;
+}
+
+void
+priq_free(struct priq *priq)
+{
+	struct priq *p;
+	struct priq *next;
+
+	for (p = priq; p != NULL; p = next) {
+		next = p->next;
+		free(p);
+	}
+}
+
