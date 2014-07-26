cca9da614d6ece77d47ef7654c0831c9b457607d
Kate F <kate@elide.org>
2014-07-26 11:47:27 +0000

    A facility for holding variable bindings.

---
+++ src/lx/var.c
@@ -0,0 +1,72 @@
+/* $Id$ */
+
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct var {
+	const char *name;
+	struct fsm *fsm;
+	struct var *next;
+};
+
+struct var *
+find(const struct var *head, const char *name)
+{
+	const struct var *p;
+
+	assert(name != NULL);
+
+	for (p = head; p != NULL; p = p->next) {
+		if (0 == strcmp(p->name, name)) {
+			return (struct var *) p;
+		}
+	}
+
+	return NULL;
+}
+
+struct var *
+var_bind(struct var **head, const char *name, struct fsm *fsm)
+{
+	struct var *new;
+
+	assert(head != NULL);
+	assert(name != NULL);
+	assert(fsm != NULL);
+
+	new = find(*head, name);
+	if (new != NULL) {
+		new->fsm = fsm;
+		return new;
+	}
+
+	new = malloc(sizeof *new);
+	if (new == NULL) {
+		return NULL;
+	}
+
+	new->name = name;
+	new->fsm  = fsm;
+
+	new->next = *head;
+	*head = new;
+
+	return new;
+}
+
+struct fsm *
+var_find(const struct var *head, const char *name)
+{
+	struct var *p;
+
+	assert(name != NULL);
+
+	p = find(head, name);
+	if (p == NULL) {
+		return NULL;
+	}
+
+	return p->fsm;
+}
+
---
+++ src/lx/var.h
@@ -0,0 +1,16 @@
+/* $Id$ */
+
+#ifndef LX_VAR_H
+#define LX_VAR_H
+
+struct var;
+struct fsm;
+
+struct var *
+var_bind(struct var **head, const char *name, struct fsm *fsm);
+
+struct fsm *
+var_find(const struct var *head, const char *name);
+
+#endif
+
