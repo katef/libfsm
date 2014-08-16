c3ecf8a8181f51d7718dae4a3d07b9d72a588884
Kate F <kate@elide.org>
2014-08-16 19:17:19 +0000

    Case.

--- src/libre/re.c
+++ src/libre/re.c
@@ -122,17 +122,17 @@ const char *
 re_strerror(enum re_err err)
 {
 	switch (err) {
-	case RE_ESUCCESS: return "success";
+	case RE_ESUCCESS: return "Success";
 	case RE_ENOMEM:   return strerror(ENOMEM);
-	case RE_EBADFORM: return "bad form";
-	case RE_EXSUB:    return "syntax error: expected sub-expression";
-	case RE_EXTERM:   return "syntax error: expected group term";
-	case RE_EXGROUP:  return "syntax error: expected group";
-	case RE_EXITEM:   return "syntax error: expected item";
-	case RE_EXCOUNT:  return "syntax error: expected count";
-	case RE_EXITEMS:  return "syntax error: expected items list";
-	case RE_EXALTS:   return "syntax error: expected alternative list";
-	case RE_EXEOF:    return "syntax error: expected EOF";
+	case RE_EBADFORM: return "Bad form";
+	case RE_EXSUB:    return "Syntax error: expected sub-expression";
+	case RE_EXTERM:   return "Syntax error: expected group term";
+	case RE_EXGROUP:  return "Syntax error: expected group";
+	case RE_EXITEM:   return "Syntax error: expected item";
+	case RE_EXCOUNT:  return "Syntax error: expected count";
+	case RE_EXITEMS:  return "Syntax error: expected items list";
+	case RE_EXALTS:   return "Syntax error: expected alternative list";
+	case RE_EXEOF:    return "Syntax error: expected EOF";
 	}
 
 	assert(!"unreached");
