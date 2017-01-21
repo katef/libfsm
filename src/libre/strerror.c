fdd5086c9a4ef2db88fdc326ceca6e089670d0e5
Kate F <kate@elide.org>
2017-01-21 14:56:53 +0000

    Missing case.

--- src/libre/strerror.c
+++ src/libre/strerror.c
@@ -14,6 +14,7 @@ re_strerror(enum re_errno e)
 	case RE_ESUCCESS:     return "Success";
 	case RE_EERRNO:       return strerror(errno);
 	case RE_EBADDIALECT:  return "Bad dialect";
+	case RE_EBADGROUP:    return "Bad group";
 
 	case RE_EOVERLAP:     return "Redundancy in group";
 	case RE_ENEGRANGE:    return "Negative group range";
