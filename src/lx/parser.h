a8dad2913fd79823ab0bfffd4271a5b8c745c0f6
Devon H. O'Dell <dho@fastly.com>
2017-02-25 13:58:24 -0800

    lx: regenerate parser

--- src/lx/parser.h
+++ src/lx/parser.h
@@ -9,7 +9,7 @@
 
 /* BEGINNING OF HEADER */
 
-#line 115 "src/lx/parser.act"
+#line 132 "src/lx/parser.act"
 
 
 	#include <stdio.h>
@@ -29,7 +29,7 @@
 extern void p_lx(lex_state, act_state, ast *);
 /* BEGINNING OF TRAILER */
 
-#line 873 "src/lx/parser.act"
+#line 885 "src/lx/parser.act"
 
 
 #line 36 "src/lx/parser.h"
