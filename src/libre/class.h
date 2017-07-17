c14eaf52d3a056340b2c6152a2189f10c997d8af
Kate F <kate@elide.org>
2017-07-17 17:13:19 +0100

    Implementation of character classes.

---
+++ src/libre/class.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2008-2017 Katherine Flavel
+ *
+ * See LICENCE for the full copyright terms.
+ */
+
+#ifndef RE_CLASS_H
+#define RE_CLASS_H
+
+struct fsm_options;
+
+struct fsm *class_alnum_fsm(const struct fsm_options *opt);
+struct fsm *class_alpha_fsm(const struct fsm_options *opt);
+struct fsm *class_ascii_fsm(const struct fsm_options *opt);
+struct fsm *class_blank_fsm(const struct fsm_options *opt);
+struct fsm *class_cntrl_fsm(const struct fsm_options *opt);
+struct fsm *class_digit_fsm(const struct fsm_options *opt);
+struct fsm *class_graph_fsm(const struct fsm_options *opt);
+struct fsm *class_lower_fsm(const struct fsm_options *opt);
+struct fsm *class_print_fsm(const struct fsm_options *opt);
+struct fsm *class_punct_fsm(const struct fsm_options *opt);
+struct fsm *class_space_fsm(const struct fsm_options *opt);
+struct fsm *class_upper_fsm(const struct fsm_options *opt);
+struct fsm *class_word_fsm(const struct fsm_options *opt);
+struct fsm *class_xdigit_fsm(const struct fsm_options *opt);
+
+#endif
+
