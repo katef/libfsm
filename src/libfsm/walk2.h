088475b77a61f960c3d173b30edf654bce00eeb3
Shannon F. Stewman <shannon.stewman@gmail.com>
2017-09-05 10:47:00 -0400

    Adds walk2 API
    The walk2 API allows the construction of a new DFA as a cross-product of
    two DFAs by simultaneously walking the two DFAs.
    
    The states and transitions of the new DFA can be restricted as follows:
    
        1) Transitions can be restricted to ones that occur in only one DFA
           or both DFAs.
    
        2) End states can be restricted to states that are end states in
           only one DFA or both DFAs.
    
    Thus, the operations of union, intersection, and subtraction can be
    implemented using fsm_walk2:
    
    intersection (A&B):
        only follow transitions that occur in both A and B, only allow end
        states if they occur in both A and B.
    
    subtraction (A-B):
        follow transitions if they occur in A or both A and B, only allow
        end states if they occur in A and not B.
    
    union (A|B):
        follow transitions that occur in either A or B (including both A&B).
        allow end states if they occur in either A or B (including both
        A&B).
    
    Implementing fsm_intersect and fsm_subtract with fsm_walk2 has the
    following benefits over the current implementation:
    
        1) Opaque values are handled correctly.  In the current
           implementation, opaque values are lost when taking the complement
           of the DFA.  This isn't the case with fsm_walk2
    
        2) The implementation should be faster in general, sometimes
           dramatically so.
    
           The current implementations require 3-4 calls to fsm_complement,
           all of which can call fsm_determinise, and a call to fsm_union.
           This requires generating several temporary DFAs.
    
           The fsm_walk2 approach should generate far fewer temporaries on
           most inputs, and should require at most two calls to
           fsm_determinise (to ensure the inputs are DFAs).

---
+++ src/libfsm/walk2.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2017 Shannon F. Stewman
+ *
+ * See LICENCE for the full copyright terms.
+ */
+
+#ifndef FSM_WALK2_H
+#define FSM_WALK2_H
+
+/* Bit values for walking two fsms together.  Used to constrain what
+ * edges will be followed and what combined states can be considered end
+ * states.
+ *
+ * There are four bits because there are four possible states (NEITHER,
+ * ONLYA, ONLYB, BOTH).  We use these bits as a fast table lookup.
+ *
+ * When walking two graphs, the caller orders them (A,B).  This is
+ * important for some non-commutative binary operations, like
+ * subtraction, where A-B != B-A.
+ *
+ * The names below assume that the order is (A,B), but make no
+ * assumptions about what that means.
+ *
+ * These values can be bitwise OR'd together.  So, to find the states
+ * for subtraction A-B, the mask would be:
+ *      FSM_WALK2_ONLYA | FSM_WALK2_BOTH
+ */
+enum {
+	FSM_WALK2_NEITHER = 1 << 0x0,   /* To complete the four possible states */
+	FSM_WALK2_ONLYB   = 1 << 0x1,   /* Follow edges that B has and A does not */
+	FSM_WALK2_ONLYA   = 1 << 0x2,   /* Follow edges that A has and B does not */
+	FSM_WALK2_BOTH    = 1 << 0x3    /* Follow edges that both A and B have */
+};
+
+struct fsm *
+fsm_walk2(struct fsm *a, struct fsm *b, unsigned edgemask, unsigned endmask);
+
+#endif /* WALK2_H */
+
