44fb58bfc9ea7b828d8fe229a041aa822475bef4
Kate F <kate@elide.org>
2017-09-09 15:56:19 +0100

    Remove state equivalence folding from `fsm_minimise()`.
    This was introduced to account for a situation whereby reversing would introduce an additional state, linking to multiple states by epsilons. The newly-added start state is not rechable by other means, and so transitions after the epsilon closure when converting NFA->DFA do not find those states equivalent.
    
    However, merging them here is the wrong thing to do; my primary argument is that `fsm -rp | fsm -dp | fsm -rp | fsm -rp` ought to behave exactly the same as `fsm_minimise()` does.
    
    So if this is reintroduced, it ought to be done inside `fsm_determinise()` instead.

--- src/libfsm/minimise.c
+++ src/libfsm/minimise.c
@@ -16,90 +16,6 @@
 
 #include "internal.h"
 
-/* TODO: explain not true equivalence; only intended for use here */
-static int
-equivalent(struct fsm_state *a, struct fsm_state *b)
-{
-	struct fsm_edge *ae, *be;
-	struct set_iter it;
-
-	assert(a != NULL);
-	assert(b != NULL);
-
-	if (set_count(a->edges) != set_count(b->edges)) {
-		return 0;
-	}
-
-	for (ae = set_first(a->edges, &it); ae != NULL; ae = set_next(&it)) {
-		if ((be = fsm_hasedge(b, ae->symbol)) == NULL) {
-			return 0;
-		}
-
-		if (!set_equal(ae->sl, be->sl)) {
-			return 0;
-		}
-	}
-
-	/* TODO: anything else? */
-
-	return 1;
-}
-
-/* Return true if the edges after o contains state */
-/* TODO: centralise */
-static int
-contains(struct set *edges, int o, struct fsm_state *state)
-{
-	struct fsm_edge *e, search;
-	struct set_iter it;
-
-	assert(edges != NULL);
-	assert(state != NULL);
-
-	search.symbol = o;
-	for (e = set_firstafter(edges, &it, &search); e != NULL; e = set_next(&it)) {
-		if (set_contains(e->sl, state)) {
-			return 1;
-		}
-	}
-
-	return 0;
-}
-
-/* Count the number of distinct states to which a state transitions */
-/* TODO: centralise? */
-static unsigned
-counttargets(struct fsm_state *state)
-{
-	struct fsm_edge *e;
-	struct set_iter it;
-	unsigned n;
-
-	assert(state != NULL);
-
-	n = 0;
-
-	for (e = set_first(state->edges, &it); e != NULL; e = set_next(&it)) {
-		struct fsm_state *s;
-
-		if (set_empty(e->sl)) {
-			continue;
-		}
-
-		/* Note that this assumes the state is a DFA state */
-		s = set_only(e->sl);
-
-		/* Distinct targets only */
-		if (contains(state->edges, e->symbol, s)) {
-			continue;
-		}
-
-		n++;
-	}
-
-	return n;
-}
-
 int
 fsm_minimise(struct fsm *fsm)
 {
@@ -129,39 +45,6 @@ fsm_minimise(struct fsm *fsm)
 		r = fsm_determinise(fsm);
 		if (!r) {
 			return 0;
-		}
-	}
-
-	/*
-	 * If the start state is equivalent to another state then merge them.
-	 * This is a special case due to neccessarily adding a state during
-	 * reversal under some situations. It's a bit of a hack and I don't
-	 * like it at all.
-	 *
-	 * This is special-case code; it can only possibly occur for the start
-	 * state.
-	 */
-	if (fsm_isend(fsm, fsm->start) || counttargets(fsm->start) > 1) {
-		struct fsm_state *s;
-		struct fsm_state *next;
-
-		for (s = fsm->sl; s != NULL; s = next) {
-			next = s->next;
-
-			if (s == fsm->start) {
-				continue;
-			}
-
-			if (equivalent(fsm->start, s)) {
-				/* XXX: I *think* there's no need to carryopaque() to s,
-				 * since newly-added start states would never have an opaque. */
-
-				fsm_removestate(fsm, fsm->start);
-
-				fsm->start = s;
-
-				break;
-			}
 		}
 	}
 
