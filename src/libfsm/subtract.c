e364b7f2324b66298c294c65df4d9d034c993afb
Shannon F. Stewman <shannon.stewman@gmail.com>
2017-09-23 15:25:58 -0400

    Moves various things around to their proper locations

--- src/libfsm/subtract.c
+++ src/libfsm/subtract.c
@@ -4,44 +4,22 @@
  * See LICENCE for the full copyright terms.
  */
 
-#include <assert.h>
 #include <stddef.h>
-#include <limits.h>
-#include <errno.h>
-
-#include <adt/set.h>
 
 #include <fsm/fsm.h>
 #include <fsm/bool.h>
 
-#include "internal.h"
+#include "walk2.h"
 
 struct fsm *
-fsm_subtract_old(struct fsm *a, struct fsm *b)
+fsm_subtract_bywalk(struct fsm *a, struct fsm *b)
 {
-	struct fsm *q;
+	return fsm_subtract(a,b);
+}
 
-	assert(a != NULL);
-	assert(b != NULL);
-
-	if (a->opt != b->opt) {
-		errno = EINVAL;
-		return NULL;
-	}
-
-	/*
-	 * a - b is implemented as: a & ~b
-	 */
-
-	if (!fsm_complement(b)) {
-		return NULL;
-	}
-
-	q = fsm_intersect(a, b);
-	if (q == NULL) {
-		return NULL;
-	}
-
-	return q;
+struct fsm *
+fsm_subtract(struct fsm *a, struct fsm *b)
+{
+	return fsm_walk2(a,b, FSM_WALK2_EDGE_SUBTRACT, FSM_WALK2_END_SUBTRACT);
 }
 
