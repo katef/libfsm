091ea3b2663d9c6e3f5986b6a15439c5de2e30d4
Kate F <kate@elide.org>
2017-10-11 21:29:14 +0200

    Clarification.

--- src/libfsm/intersect.c
+++ src/libfsm/intersect.c
@@ -19,6 +19,17 @@ enum { FSM_WALK2_END_INTERSECT  = FSM_WALK2_BOTH };
 struct fsm *
 fsm_intersect(struct fsm *a, struct fsm *b)
 {
+	/*
+	 * This is intersection implemented by walking sets of states through
+	 * both FSM simultaneously, as described by Hopcroft, Motwani and Ullman
+	 * (2001, 2nd ed.) 4.2, Closure under Intersection.
+	 *
+	 * This could also be done in terms of complements and union,
+	 * by DeMorgan's theorem:
+	 *
+	 *     a \n b = ~(~a \u ~b)
+	 */
+
 	return fsm_walk2(a,b, FSM_WALK2_EDGE_INTERSECT, FSM_WALK2_END_INTERSECT);
 }
 
