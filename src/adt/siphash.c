885f5320ceabe0e00fe4d1eedbac6a600b82b1d5
Kate F <kate@elide.org>
2019-03-24 04:08:05 +0000

    `/* FALLTHROUGH */` to appease gcc.

--- src/adt/siphash.c
+++ src/adt/siphash.c
@@ -112,18 +112,25 @@ int siphash(const uint8_t *in, const size_t inlen, const uint8_t *k,
     switch (left) {
     case 7:
         b |= ((uint64_t)in[6]) << 48;
+		/* FALLTHROUGH */
     case 6:
         b |= ((uint64_t)in[5]) << 40;
+		/* FALLTHROUGH */
     case 5:
         b |= ((uint64_t)in[4]) << 32;
+		/* FALLTHROUGH */
     case 4:
         b |= ((uint64_t)in[3]) << 24;
+		/* FALLTHROUGH */
     case 3:
         b |= ((uint64_t)in[2]) << 16;
+		/* FALLTHROUGH */
     case 2:
         b |= ((uint64_t)in[1]) << 8;
+		/* FALLTHROUGH */
     case 1:
         b |= ((uint64_t)in[0]);
+		/* FALLTHROUGH */
         break;
     case 0:
         break;
