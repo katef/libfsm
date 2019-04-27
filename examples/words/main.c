0c33037aac8b75e6fd1414bae66189359f9aa27a
Shannon F. Stewman <shannon.stewman@gmail.com>
2019-04-27 03:54:30 -0400

    Adds Aho-Corasick support to example/words

--- examples/words/main.c
+++ examples/words/main.c
@@ -31,6 +31,15 @@ int main(int argc, char *argv[]) {
 	unsigned long ms, mt;
 	int timing;
 	int native = 0;
+	int ahocorasick = 0;
+	int unanchored = 0;
+	int single_match = 1;
+
+	struct {
+		char **list;
+		size_t len;
+		size_t cap;
+	} words = { NULL, 0, 0 };
 
 	opt.anonymous_states  = 1;
 	opt.consolidate_edges = 1;
@@ -42,12 +51,15 @@ int main(int argc, char *argv[]) {
 	{
 		int c;
 
-		while (c = getopt(argc, argv, "h" "ndmct"), c != -1) {
+		while (c = getopt(argc, argv, "h" "aNndmMct"), c != -1) {
 			switch (c) {
+			case 'a': ahocorasick = 1;       break;
 			case 'd': dmf = fsm_determinise; break;
 			case 'm': dmf = fsm_minimise;    break;
 
+			case 'M': single_match = 0;      break;
 			case 'n': native = 1;            break;
+			case 'N': unanchored = 1;        break;
 
 			case 'c': print = fsm_print_dot; break;
 
@@ -70,16 +82,28 @@ int main(int argc, char *argv[]) {
 		goto usage;
 	}
 
-	fsm = fsm_new(&opt);
-	if (fsm == NULL) {
-		perror("fsm_new");
-		return 1;
-	}
+	fsm = NULL;
+	start = NULL;
 
-	start = fsm_addstate(fsm);
-	if (start == NULL) {
-		perror("fsm_addtate");
-		return 1;
+	if (ahocorasick) {
+		words.cap = 1024;
+		words.list = malloc(words.cap * sizeof(words.list[0]));
+		if (words.list == NULL) {
+			perror("allocating word list");
+			exit(EXIT_FAILURE);
+		}
+	} else {
+		fsm = fsm_new(&opt);
+		if (fsm == NULL) {
+			perror("fsm_new");
+			return 1;
+		}
+
+		start = fsm_addstate(fsm);
+		if (start == NULL) {
+			perror("fsm_addtate");
+			return 1;
+		}
 	}
 
 	ms = 0;
@@ -97,6 +121,47 @@ int main(int argc, char *argv[]) {
 		if (-1 == clock_gettime(CLOCK_MONOTONIC, &pre)) {
 			perror("clock_gettime");
 			exit(EXIT_FAILURE);
+		}
+
+		if (ahocorasick) {
+			char *w;
+			size_t wlen;
+
+			if (words.len >= words.cap) {
+				size_t newcap;
+				char **wl;
+
+				if (words.cap < 16384) {
+					newcap = words.cap * 2;
+				} else {
+					newcap = words.cap + words.cap/4;
+				}
+
+				wl = malloc(newcap * sizeof wl[0]);
+				if (wl == NULL) {
+					perror("adding word");
+					exit(EXIT_FAILURE);
+				}
+
+				fprintf(stderr, "words.len = %zu, words.cap = %zu, newcap = %zu, words.list=%p, wl=%p\n",
+					words.len, words.cap, newcap, (void *)words.list, (void *)wl);
+				memcpy(wl, words.list, words.len * sizeof words.list[0]);
+				free(words.list);
+
+				words.list = wl;
+				words.cap = newcap;
+			}
+
+			wlen = strlen(p);
+			w = malloc(wlen+1);
+			if (w == NULL) {
+				perror("adding word");
+				exit(EXIT_FAILURE);
+			}
+			memcpy(w,p,wlen+1);
+
+			words.list[words.len++] = w;
+			continue;
 		}
 
 		r = re_comp(native ? RE_NATIVE : RE_LITERAL, fsm_sgetc, &p, &opt, 0, &e);
@@ -128,7 +193,30 @@ int main(int argc, char *argv[]) {
 		           + ((long) post.tv_nsec - (long) pre.tv_nsec) / 1000000;
 	}
 
-	fsm_setstart(fsm, start);
+	if (ahocorasick) {
+		struct timespec pre, post;
+
+		if (-1 == clock_gettime(CLOCK_MONOTONIC, &pre)) {
+			perror("clock_gettime");
+			exit(EXIT_FAILURE);
+		}
+
+		fsm = re_strings(&opt, (const char **)words.list, words.len, unanchored ? 0 :  (RE_STRINGS_ANCHOR_LEFT | RE_STRINGS_ANCHOR_RIGHT));
+		if (fsm == NULL) {
+			perror("converting trie to fsm");
+			exit(EXIT_FAILURE);
+		}
+
+		if (-1 == clock_gettime(CLOCK_MONOTONIC, &post)) {
+			perror("clock_gettime");
+			exit(EXIT_FAILURE);
+		}
+
+		mt += 1000 * (post.tv_sec - pre.tv_sec)
+		           + ((long) post.tv_nsec - (long) pre.tv_nsec) / 1000000;
+	} else {
+		fsm_setstart(fsm, start);
+	}
 
 	if (dmf != NULL) {
 		struct timespec pre, post;
