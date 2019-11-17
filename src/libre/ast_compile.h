f27bd246b8ea0e7c4324f37b8ff6df522c31d9bc
Kate F <kate@elide.org>
2019-11-16 23:41:58 -0800

    Fold in entry point for AST compilation.
    There's no need to keep this separate now we have only one kind of AST node.

--- src/libre/ast_compile.h
+++ src/libre/ast_compile.h
@@ -11,19 +11,12 @@
  * Compilation to FSM
  */
 
-struct ast_expr;
 struct fsm;
 struct re_err;
 
-int
-ast_compile_expr(struct ast_expr *n,
-	struct fsm *fsm, enum re_flags flags,
-	struct re_err *err,
-	struct fsm_state *x, struct fsm_state *y);
-
 struct fsm *
 ast_compile(const struct ast *ast,
-	enum re_flags flags,
+	enum re_flags re_flags,
 	const struct fsm_options *opt,
 	struct re_err *err);
 
