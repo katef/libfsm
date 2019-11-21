f12a62ab41c3e3c68e2dd3ae95ea610573ac8b7e
Kate F <kate@elide.org>
2019-11-20 16:51:38 -0800

    Typo; the SQL `[:SPACE:]` class should be constructing a spchr named class node.
    This is confusing thanks to the naming between dialects, so I added some commentary and a regression test.

--- src/libre/dialect/sql/re_dialect_sql.c
+++ src/libre/dialect/sql/re_dialect_sql.c
@@ -11,6 +11,14 @@
 #include "../../class.h"
 #include "../../class_lookup.h"
 
+/*
+ * SQL 2003 ISO/IEC 9075-2:2003 (E) 8.6.3 q)
+ *   [:SPACE:] is "the set of all character strings of length 1 (one)
+ *   that are the <space> character."
+ *
+ * Not to be confused with [:space:] in other dialects, which is
+ * written [:WHITESPACE:] in SQL.
+ */
 static const struct {
 	const char *name;
 	class_constructor *ctor;
@@ -19,7 +27,7 @@ static const struct {
 	{ "[:ALPHA:]", class_alpha_fsm },
 	{ "[:DIGIT:]", class_digit_fsm },
 	{ "[:LOWER:]", class_lower_fsm },
-	{ "[:SPACE:]", class_space_fsm },
+	{ "[:SPACE:]", class_spchr_fsm },
 	{ "[:UPPER:]", class_upper_fsm },
 	{ "[:WHITESPACE:]", class_space_fsm }
 };
---
+++ tests/sql/in10.re
@@ -0,0 +1,1 @@
+[[:SPACE:]][[:WHITESPACE:]]
