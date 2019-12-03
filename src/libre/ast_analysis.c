448a7bc887705b7700f83dc42a3a1d7c44342239
Shannon F. Stewman <shannon.stewman@gmail.com>
2019-12-03 17:26:57 -0500

    Fixes /^/ and /$/

--- src/libre/ast_analysis.c
+++ src/libre/ast_analysis.c
@@ -173,6 +173,10 @@ always_consumes_input(const struct ast_expr *n, int thud)
 	case AST_EXPR_NAMED:
 		return 1;
 
+	case AST_EXPR_SUBTRACT:
+		/* FIXME: need to perform the subtraction to know */
+		return 1;
+
 	case AST_EXPR_CONCAT: {
 		size_t i;
 
@@ -420,9 +424,7 @@ assign_firsts(struct ast_expr *n)
 		break;
 
 	case AST_EXPR_ANCHOR:
-		if (n->u.anchor.type == AST_ANCHOR_START) {
-			set_flags(n, AST_FLAG_FIRST);
-		}
+		set_flags(n, AST_FLAG_FIRST);
 		break;
 
 	case AST_EXPR_LITERAL:
