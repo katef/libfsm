ebc3fe037d156e8c286b5b50f99dc0707c82437c
Shannon F. Stewman <shannon.stewman@gmail.com>
2020-07-04 13:09:44 -0400

    Fixes 'notword' class to exclude [a-z]

--- src/libre/class/notword.c
+++ src/libre/class/notword.c
@@ -6,7 +6,8 @@ static const struct range ranges[] = {
 	{ 0x0000UL, 0x002FUL },
 	{ 0x003AUL, 0x0040UL },
 	{ 0x005BUL, 0x005EUL },
-	{ 0x0060UL, 0x00FFUL }
+	{ 0x0060UL, 0x0060UL },
+	{ 0x007BUL, 0x00FFUL }
 };
 
 const struct class class_notword = {
