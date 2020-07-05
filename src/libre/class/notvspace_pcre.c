fc6381a1a35596838e37fa39d13d27ac8f3dc5eb
Kate F <kate@elide.org>
2020-07-04 18:49:48 -0700

    Missing generated character classes.

---
+++ src/libre/class/notvspace_pcre.c
@@ -0,0 +1,15 @@
+/* generated */
+
+#include "class.h"
+
+static const struct range ranges[] = {
+	{ 0x0000UL, 0x0009UL },
+	{ 0x000EUL, 0x0084UL },
+	{ 0x0086UL, 0x00FFUL }
+};
+
+const struct class class_notvspace_pcre = {
+	ranges,
+	sizeof ranges / sizeof *ranges
+};
+
