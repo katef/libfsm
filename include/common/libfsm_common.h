32c23749becdede47f7c1540478f52ac11de7d59
Scott Vokes <svokes@fastly.com>
2021-03-18 12:29:42 -0400

    Add a header w/ macro to suppress unsigned integer UBSan warnings.
    This header can be included by all of the various src/* codebases.

---
+++ include/common/libfsm_common.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2021 Scott Vokes
+ *
+ * See LICENCE for the full copyright terms.
+ */
+
+#ifndef LIBFSM_COMMON_H
+#define LIBFSM_COMMON_H
+
+#if defined(__clang__)
+/* Newer versions of clang's UBSan emit warnings about *all* unsigned
+ * integer overflows. While they are defined behavior, overflow can
+ * cause bugs. This macro ignores them for a particular function.
+ * Overflow/rollover is expected in when hashing, for example. */
+#define SUPPRESS_EXPECTED_UNSIGNED_INTEGER_OVERFLOW()		\
+	__attribute__((no_sanitize("integer")))
+#else
+#define SUPPRESS_EXPECTED_UNSIGNED_INTEGER_OVERFLOW()
+#endif
+
+#endif
