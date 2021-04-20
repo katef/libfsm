b4a98c22f3fc770350af78cb402599b5a70cebbd
Federico Mena Quintero <federico@gnome.org>
2021-04-20 16:16:52 -0500

    Move all of start.c to Rust
    The strategy for this commit:
    
    * Implement clear_start / set_start / get_start in Rust as methods,
    with argument checks as assertions.
    
    * Implement a C ABI wrapper to match the fsm_clearstart() /
    etc. functions.  The non-null argument checks go there.
    
    * Test both sets of functions right there.  We cannot construct a Fsm
    in Rust just yet, so the tests hack up just enough initial state for
    the code to run; this can be removed later.
    
    * Remove start.c.
    
    Later we can move all of the C API to a c_api.rs or whatever.

--- src/libfsm/fsm.rs
+++ src/libfsm/fsm.rs
@@ -39,7 +39,7 @@ struct State {
 /// Finite State Machine.
 // Keep in sync with interanl.h:struct fsm
 #[repr(C)]
-struct Fsm {
+pub struct Fsm {
     /// Array.
     states: *mut State,
 
@@ -57,4 +57,115 @@ struct Fsm {
     capture_info: CaptureInfo,
     endid_info: EndIdInfo,
     opt: Options,
+}
+
+impl Fsm {
+    pub fn clear_start(&mut self) {
+        self.hasstart = false;
+    }
+
+    pub fn set_start(&mut self, state: StateId) {
+        assert!((state as usize) < self.statecount);
+
+        self.start = state;
+        self.hasstart = true;
+    }
+
+    pub fn get_start(&self) -> Option<StateId> {
+        if self.hasstart {
+            assert!((self.start as usize) < self.statecount);
+            Some(self.start)
+        } else {
+            None
+        }
+    }
+}
+
+#[no_mangle]
+pub unsafe fn fsm_clearstart(fsm: *mut Fsm) {
+    assert!(!fsm.is_null());
+    let fsm = &mut *fsm;
+
+    fsm.clear_start();
+}
+
+#[no_mangle]
+pub unsafe fn fsm_setstart(fsm: *mut Fsm, state: StateId) {
+    assert!(!fsm.is_null());
+    let fsm = &mut *fsm;
+
+    fsm.set_start(state);
+}
+
+#[no_mangle]
+pub unsafe fn fsm_getstart(fsm: *const Fsm, out_start: *mut StateId) -> i32 {
+    assert!(!fsm.is_null());
+    assert!(!out_start.is_null());
+
+    let fsm = &*fsm;
+    match fsm.get_start() {
+        Some(id) => {
+            *out_start = id;
+            1
+        }
+
+        None => 0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::ptr;
+    use super::*;
+
+    // Just here until we can construct Fsm in Rust
+    fn dummy_fsm_new() -> Fsm {
+        Fsm {
+            states: ptr::null_mut(),
+            statealloc: 0,
+            statecount: 0,
+            endcount: 0,
+            start: 0,
+            hasstart: false,
+            capture_info: ptr::null_mut(),
+            endid_info: ptr::null_mut(),
+            opt: ptr::null(),
+        }
+    }
+
+    #[test]
+    fn sets_and_clears_start() {
+        let mut fsm = dummy_fsm_new();
+
+        // FIXME: this sets up an inconsistent state, but we need it to
+        // test the assertion inside fsm.set_start().
+        fsm.statecount = 1;
+
+        fsm.set_start(0);
+        assert_eq!(fsm.get_start(), Some(0));
+
+        fsm.clear_start();
+        assert!(fsm.get_start().is_none())
+    }
+
+    #[test]
+    fn sets_and_clears_start_from_c() {
+        let mut fsm = dummy_fsm_new();
+        let fsm_ptr = &mut fsm as *mut _;
+
+        // FIXME: this sets up an inconsistent state, but we need it to
+        // test the assertion inside fsm_setstart().
+        fsm.statecount = 1;
+
+        unsafe {
+            let mut n = 0;
+
+            fsm_setstart(fsm_ptr, 0);
+            assert_eq!(fsm_getstart(fsm_ptr, &mut n), 1);
+            assert_eq!(n, 0);
+
+            fsm_clearstart(fsm_ptr);
+            assert_eq!(fsm_getstart(fsm_ptr, &mut n), 0);
+        }
+    }
 }
