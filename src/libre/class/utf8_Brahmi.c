a6fa5df45b14e56650143718fc05a3b09a7853c4
data-man <dataman@tutanota.com>
2021-09-17 08:35:12 +0500

    Update to Unicode 14.0

--- src/libre/class/utf8_Brahmi.c
+++ src/libre/class/utf8_Brahmi.c
@@ -4,7 +4,7 @@
 
 static const struct range ranges[] = {
 	{ 0x11000UL, 0x1104DUL },
-	{ 0x11052UL, 0x1106FUL },
+	{ 0x11052UL, 0x11075UL },
 	{ 0x1107FUL, 0x1107FUL }
 };
 
--- src/libre/class/utf8_Tangsa.c
+++ src/libre/class/utf8_Tangsa.c
@@ -3,11 +3,11 @@
 #include "class.h"
 
 static const struct range ranges[] = {
-	{ 0x1700UL, 0x1715UL },
-	{ 0x171FUL, 0x171FUL }
+	{ 0x16A70UL, 0x16ABEUL },
+	{ 0x16AC0UL, 0x16AC9UL }
 };
 
-const struct class utf8_Tagalog = {
+const struct class utf8_Tangsa = {
 	ranges,
 	sizeof ranges / sizeof *ranges
 };
