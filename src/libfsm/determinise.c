090a9b61a294515a4615f17239ea7db26c2b2b36
Scott Vokes <svokes@fastly.com>
2021-11-05 16:36:09 -0400

    Code style: Inline `*_internal.h` headers in their .c files.

--- src/libfsm/determinise.c
+++ src/libfsm/determinise.c
@@ -4,7 +4,228 @@
  * See LICENCE for the full copyright terms.
  */
 
-#include "determinise_internal.h"
+#include <assert.h>
+#include <stddef.h>
+#include <string.h>
+#include <errno.h>
+
+#include <fsm/fsm.h>
+#include <fsm/capture.h>
+#include <fsm/pred.h>
+#include <fsm/walk.h>
+
+#include <adt/alloc.h>
+#include <adt/set.h>
+#include <adt/hash.h>
+#include <adt/edgeset.h>
+#include <adt/stateset.h>
+#include <adt/internedstateset.h>
+#include <adt/ipriq.h>
+#include <adt/u64bitset.h>
+
+#include "internal.h"
+#include "capture.h"
+#include "endids.h"
+
+#include <ctype.h>
+
+#define DUMP_MAPPING 0
+#define LOG_DETERMINISE_CLOSURES 0
+#define LOG_DETERMINISE_CAPTURES 0
+#define LOG_SYMBOL_CLOSURE 0
+#define LOG_AC 0
+
+#if LOG_DETERMINISE_CAPTURES
+#include <fsm/print.h>
+#endif
+
+/* Starting number of buckets for the map's
+ * hash table. Must be a power of 2. */
+#define DEF_MAP_BUCKET_CEIL 4
+
+/*
+ * This maps a DFA state onto its associated NFA symbol closure, such that an
+ * existing DFA state may be found given any particular set of NFA states
+ * forming a symbol closure.
+ *
+ * Since the same state sets tend to be constructed over and over during
+ * determinization, they are interned -- constructing a set with the same
+ * contents will instead get a reference to an existing instance.
+ */
+struct map {
+	const struct fsm_alloc *alloc;
+
+	size_t used;
+	size_t count;
+
+	/* These are allocated on demand, because they're pushed onto the
+	 * stack by address, and *edges is used by address. */
+	struct mapping {
+		interned_state_set_id iss;
+		size_t dfastate;
+		struct edge_set *edges;
+	} **buckets;
+};
+
+struct map_iter {
+	struct map *m;
+	size_t i;
+};
+
+struct reverse_mapping {
+	unsigned count;
+	unsigned ceil;
+	fsm_state_t *list;
+};
+
+struct det_copy_capture_actions_env {
+	char tag;
+	struct fsm *dst;
+	struct reverse_mapping *reverse_mappings;
+	int ok;
+};
+
+#define MAPPINGSTACK_DEF_CEIL 16
+struct mappingstack {
+	const struct fsm_alloc *alloc;
+	size_t ceil;
+	size_t depth;
+	struct mapping **s;
+};
+
+#define AC_NO_STATE ((fsm_state_t)-1)
+#define DEF_ITER_CEIL 4
+#define DEF_GROUP_CEIL 4
+#define DEF_OUTPUT_CEIL 4
+#define DEF_DST_CEIL 4
+
+struct analyze_closures_env {
+	const struct fsm_alloc *alloc;
+	const struct fsm *fsm;
+	struct interned_state_set_pool *issp;
+
+	/* TODO: iters and outputs are never in use at the same time, so
+	 * they could potentially share the same underlying allocation.
+	 * It may not be worth the extra complexity though. */
+
+	/* Temporary state for iterators */
+	size_t iter_ceil;
+	size_t iter_count;
+	struct ac_iter {
+		/* if info.to is AC_NO_STATE then it's done */
+		struct edge_group_iter iter;
+		struct edge_group_iter_info info;
+	} *iters;
+
+	struct ipriq *pq;
+
+	/* All sets of labels leading to states,
+	 * stored in ascending order. */
+	size_t group_ceil;
+	size_t group_count;
+	struct ac_group {
+		fsm_state_t to;
+		/* words_used & (1U << x) -> labels[x] has bits set */
+		uint8_t words_used;
+		uint64_t labels[256/64];
+	} *groups;
+
+	/* A collection of (label set -> interned state set) pairs. */
+	size_t output_ceil;
+	size_t output_count;
+	struct ac_output {
+		uint64_t labels[256/64];
+		interned_state_set_id iss;
+	} *outputs;
+
+	size_t dst_ceil;
+	fsm_state_t *dst;
+};
+
+static int
+analyze_closures_for_iss(struct analyze_closures_env *env,
+	interned_state_set_id curr_iss);
+
+static int
+analyze_closures__init_iterators(struct analyze_closures_env *env,
+	const struct state_set *ss, size_t set_count);
+
+static int
+analyze_closures__init_groups(struct analyze_closures_env *env);
+
+enum ac_collect_res {
+	AC_COLLECT_DONE,
+	AC_COLLECT_EMPTY,
+	AC_COLLECT_ERROR = -1,
+};
+
+static enum ac_collect_res
+analyze_closures__collect(struct analyze_closures_env *env);
+
+static int
+analyze_closures__analyze(struct analyze_closures_env *env);
+
+static int
+analyze_closures__save_output(struct analyze_closures_env *env,
+    const uint64_t labels[256/4], interned_state_set_id iss);
+
+static int
+analyze_closures__grow_iters(struct analyze_closures_env *env,
+    size_t set_count);
+
+static int
+analyze_closures__grow_groups(struct analyze_closures_env *env);
+
+static int
+analyze_closures__grow_dst(struct analyze_closures_env *env);
+
+static int
+analyze_closures__grow_outputs(struct analyze_closures_env *env);
+
+static int
+map_add(struct map *map,
+	fsm_state_t dfastate, interned_state_set_id iss, struct mapping **new_mapping);
+
+static int
+map_find(const struct map *map, interned_state_set_id iss,
+	struct mapping **mapping);
+
+static void
+map_free(struct map *map);
+
+static struct mapping *
+map_first(struct map *map, struct map_iter *iter);
+
+static struct mapping *
+map_next(struct map_iter *iter);
+
+static int
+add_reverse_mapping(const struct fsm_alloc *alloc,
+	struct reverse_mapping *reverse_mappings,
+	fsm_state_t dfastate, fsm_state_t nfa_state);
+
+static int
+det_copy_capture_actions(struct reverse_mapping *reverse_mappings,
+	struct fsm *dst, struct fsm *src);
+
+static int
+grow_map(struct map *map);
+
+static int
+remap_capture_actions(struct map *map, struct interned_state_set_pool *issp,
+	struct fsm *dst_dfa, struct fsm *src_nfa);
+
+static struct mappingstack *
+stack_init(const struct fsm_alloc *alloc);
+
+static void
+stack_free(struct mappingstack *stack);
+
+static int
+stack_push(struct mappingstack *stack, struct mapping *item);
+
+static struct mapping *
+stack_pop(struct mappingstack *stack);
 
 static void
 dump_labels(FILE *f, const uint64_t labels[4])
--- src/libre/ast_compile.c
+++ src/libre/ast_compile.c
@@ -4,7 +4,163 @@
  * See LICENCE for the full copyright terms.
  */
 
-#include "ast_compile_internal.h"
+#include <assert.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <ctype.h>
+
+#include <fsm/fsm.h>
+#include <fsm/bool.h>
+#include <fsm/capture.h>
+#include <fsm/pred.h>
+#include <fsm/subgraph.h>
+
+#include <re/re.h>
+
+#include <adt/idmap.h>
+
+#include "class.h"
+#include "ast.h"
+#include "ast_compile.h"
+
+#include "libfsm/internal.h" /* XXX */
+
+#define LOG_LINKAGE 0
+#define LOG_CAPTURE_PATHS 0
+#define LOG_TRAMPOLINE 0
+
+enum link_side {
+	LINK_START,
+	LINK_END
+};
+
+/*
+ * How should this state be linked for the relevant state?
+ * Note: These are not mutually exclusive!
+ *
+ * - LINK_TOP_DOWN
+ *   Use the passed in start/end states (x and y)
+ *
+ * - LINK_GLOBAL
+ *   Link to the global start/end state (env->start_outer or env->end_outer),
+ *   because this node has a ^ or $ anchor
+ *
+ * - LINK_GLOBAL_SELF_LOOP
+ *   Link to the unanchored self loop adjacent to the start/end
+ *   states (env->start_any_loop or env->end_any_loop), because
+ *   this node is in a FIRST or LAST position, but unanchored.
+ */
+enum link_types {
+	LINK_TOP_DOWN         = 1 << 0,
+	LINK_GLOBAL           = 1 << 1,
+	LINK_GLOBAL_SELF_LOOP = 1 << 2,
+
+	LINK_NONE = 0x00
+};
+
+/* Call stack for AST -> NFA conversion.
+ *
+ * This is executed depth first and could otherwise use the C call
+ * stack, but it needs to support forking execution in order to handle
+ * AST_EXPR_ALTs where ALT subtrees have different capture behavior.
+ * Passing through AST_EXPR_GROUP nodes affect the remainder of
+ * evaluation (even after returning from their subtree), because they
+ * modify which captures will be associated with particular end states.
+ * When they only appear in some ALTs we need to fork the remainder of
+ * execution, including evaluating everything pending on the call stack
+ * multiple times. */
+#define DEF_COMP_STACK_CEIL 4
+struct comp_stack {
+	size_t ceil;
+	size_t depth;		/* 0 -> empty */
+
+	fsm_state_t end_inner;
+	fsm_state_t end_outer;
+	fsm_state_t end_any_loop;
+	fsm_state_t end_any_inner;
+	int have_end_any_loop;
+
+	struct comp_stack_frame {
+		struct ast_expr *n;
+		fsm_state_t x;
+		fsm_state_t y;
+		unsigned step;
+
+		union {
+			struct {
+				fsm_state_t link;
+			} concat;
+			struct {
+				unsigned count;
+			} alt;
+			struct {
+				struct fsm_subgraph subgraph;
+				fsm_state_t na;
+				fsm_state_t nz;
+			} repeat;
+		} u;
+	} *frames;
+};
+
+struct comp_env {
+	const struct fsm_alloc *alloc;
+	struct fsm *fsm;
+	enum re_flags re_flags;
+	struct re_err *err;
+
+	/*
+	 * These are saved so that dialects without implicit
+	 * anchoring can create states with '.' edges to self
+	 * on demand, and link them to the original start and
+	 * end states.
+	 *
+	 * Also, some states in a first/last context need to link
+	 * directly to the overall start/end states, either in
+	 * place of or along with the adjacent states.
+	 *
+	 * The inner start and end states are considered inside of
+	 * match group 0, outer are not.
+	 */
+	fsm_state_t start_outer;
+	fsm_state_t start_inner;
+	fsm_state_t start_any_loop;
+	fsm_state_t start_any_inner;
+	int have_start_any_loop;
+};
+
+static int
+comp_iter_trampoline(struct comp_env *env,
+	fsm_state_t x, struct ast_expr *n);
+
+static int
+eval_stack_frame(struct comp_env *env, struct comp_stack *stack);
+
+static int
+eval_EMPTY(struct comp_env *env, struct comp_stack *stack);
+static int
+eval_CONCAT(struct comp_env *env, struct comp_stack *stack);
+static int
+eval_ALT(struct comp_env *env, struct comp_stack *stack);
+static int
+eval_LITERAL(struct comp_env *env, struct comp_stack *stack);
+static int
+eval_CODEPOINT(struct comp_env *env, struct comp_stack *stack);
+static int
+eval_REPEAT(struct comp_env *env, struct comp_stack *stack);
+static int
+eval_GROUP(struct comp_env *env, struct comp_stack *stack);
+static int
+eval_ANCHOR(struct comp_env *env, struct comp_stack *stack);
+static int
+eval_SUBTRACT(struct comp_env *env, struct comp_stack *stack);
+static int
+eval_RANGE(struct comp_env *env, struct comp_stack *stack);
+static int
+eval_TOMBSTONE(struct comp_env *env, struct comp_stack *stack);
 
 static int
 utf8(uint32_t cp, char c[])
