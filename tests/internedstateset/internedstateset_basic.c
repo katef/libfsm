e9c237726231f8c4d8726c0cb4abeed08b7a02f7
Scott Vokes <svokes@fastly.com>
2021-11-30 17:02:50 -0500

    internedstateset: Update test to add shuffling, parameters.
    This doesn't change the implementation, but gives a basis for
    comparison. `build/tests/internedstateset/run_basic` can be called
    with `VERBOSITY`, `LIMIT`, and/or `SHUFFLE` set in the environment,
    for example:
    
        $ env LIMIT=2000 SHUFFLE=1 \
        /usr/bin/time build/tests/internedstateset/run_basic
    
    to run the test with test input of [0..1999] and compare building
    the array ascending, descending, and shuffled once.

--- tests/internedstateset/internedstateset_basic.c
+++ tests/internedstateset/internedstateset_basic.c
@@ -6,70 +6,165 @@
 
 #include <assert.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 #include <fsm/fsm.h>
 
 #include <adt/internedstateset.h>
 
+#define USE_CONFLUENT_ISS 0
+
+static void
+reverse(size_t length, fsm_state_t *states)
+{
+	size_t i;
+	for (i = 0; i < length/2; i++) {
+		const size_t rev_i = length - i - 1;
+		const fsm_state_t tmp = states[rev_i];
+		states[rev_i] = states[i];
+		states[i] = tmp;
+	}
+}
+
+static void
+fisher_yates_shuffle(size_t length, fsm_state_t *states, unsigned seed)
+{
+	size_t i;
+	srandom(seed);
+	assert(length > 1);
+	for (i = 0; i < length - 1; i++) {
+		/* pick j: i <= j < n */
+		const size_t j = i + (random() % (length - i));
+
+		const fsm_state_t tmp = states[i];
+		assert(j < length);
+		states[i] = states[j];
+		states[j] = tmp;
+	}
+}
+
+static struct interned_state_set *
+add_states_from_array(struct interned_state_set_pool *issp,
+    size_t length, const fsm_state_t *states, unsigned verbosity)
+{
+	size_t i;
+	struct interned_state_set *empty = interned_state_set_empty(issp);
+	struct interned_state_set *cur, *prev;
+#if USE_CONFLUENT_ISS
+	assert(empty == NULL);
+#endif
+
+	cur = empty;
+
+	for (i = 0; i < length; i++) {
+		const fsm_state_t s = states[i];
+		if (verbosity > 1) {
+			fprintf(stderr, "-- %zu: adding %d\n", i, s);
+		}
+		prev = cur;
+
+		/* This is handing off the older reference. */
+		cur = interned_state_set_add(issp, cur, s);
+
+		assert(cur != NULL);
+	}
+
+	if (verbosity > 0) {
+		fprintf(stderr, "== %s: done\n", __func__);
+		/* interned_state_set_dump(issp, cur); */
+	}
+
+	return cur;
+}
+
+#define DEF_VERBOSITY 0
+static unsigned
+get_verbosity(void)
+{
+	const char *v = getenv("VERBOSITY");
+	return (v == NULL ? DEF_VERBOSITY : atoi(v));
+}
+
+#define DEF_LIMIT 100
+static unsigned
+get_limit(void)
+{
+	const char *v = getenv("LIMIT");
+	return (v == NULL ? DEF_LIMIT : atoi(v));
+}
+
+#define DEF_SHUFFLE_CYCLES 10
+static unsigned
+get_shuffle_cycles(void)
+{
+	const char *v = getenv("SHUFFLE");
+	return (v == NULL ? DEF_SHUFFLE_CYCLES : atoi(v));
+}
+
 int main(void) {
-	struct interned_state_set *empty;
-	struct interned_state_set *cur, *set_up, *set_up2, *set_down, *set_down2;
+	struct interned_state_set *set_up, *set_up2, *set_down, *set_down2;
 	fsm_state_t i;
-	const size_t limit = 1000;
+	const size_t limit = get_limit();
+	const size_t shuffle_cycles = get_shuffle_cycles();
+	fsm_state_t *states;
+	const unsigned verbosity = get_verbosity();
 
 	struct interned_state_set_pool *issp = interned_state_set_pool_alloc(NULL);
 	assert(issp);
 
-	empty = interned_state_set_empty(issp);
-	assert(empty != NULL);
+	states = malloc(limit * sizeof(states[0]));
+	assert(states != NULL);
+
+	for (i = 0; i < limit; i++) {
+		states[i] = i;
+	}
 
 	/* Create state sets with [], [0], [0, 1], ... [0, ... limit-1] */
-	cur = empty;
-	for (i = 0; i < limit; i++) {
-		cur = interned_state_set_add(issp, cur, i);
-		assert(cur != NULL);
-	}
-	set_up = cur;
+	set_up = add_states_from_array(issp, limit, states, verbosity);
 
 	/* Create the same ones again */
-	cur = empty;
-	for (i = 0; i < limit; i++) {
-		cur = interned_state_set_add(issp, cur, i);
-		assert(cur != NULL);
-	}
-	set_up2 = cur;
+	set_up2 = add_states_from_array(issp, limit, states, verbosity);
 
 	/* Check that they match. */
 	assert(set_up == set_up2);
+#if USE_CONFLUENT_ISS
+	interned_state_set_release(issp, set_up2);
+#endif
 
 	/* Create state sets [], [limit-1], [limit-2, limit-1], ... */
-	cur = empty;
-	i = limit;
-	while (i > 0) {
-		i--;
-		cur = interned_state_set_add(issp, cur, i);
-		assert(cur != NULL);
-	}
-	set_down = cur;
+	reverse(limit, states);
+	set_down = add_states_from_array(issp, limit, states, verbosity);
 
 	/* Both should end up with the same state set once all are added. */
 	assert(set_up == set_down);
 
 	/* Create the state set in descending order again. These should all
 	 * be cached. */
-	cur = empty;
-	i = limit;
-	while (i > 0) {
-		i--;
-		cur = interned_state_set_add(issp, cur, i);
-		assert(cur != NULL);
-	}
-	set_down2 = cur;
+	set_down2 = add_states_from_array(issp, limit, states, verbosity);
 
 	/* These should also be pointer-equal. */
 	assert(set_down2 == set_down);
+#if USE_CONFLUENT_ISS
+	interned_state_set_release(issp, set_down);
+	interned_state_set_release(issp, set_down2);
+#endif
 
-	/* Free the pool. Check that no memory leaked. */
+	/* Repeatedly shuffle with different seeds and compare */
+	for (i = 0; i < shuffle_cycles; i++) {
+		struct interned_state_set *shuffled;
+		fisher_yates_shuffle(limit, states, i);
+		shuffled = add_states_from_array(issp, limit, states, verbosity);
+		assert(shuffled == set_up);
+#if USE_CONFLUENT_ISS
+		interned_state_set_release(issp, shuffled);
+#endif
+	}
+
+#if USE_CONFLUENT_ISS
+	interned_state_set_release(issp, set_up);
+#endif
+
 	interned_state_set_pool_free(issp);
+	free(states);
 	return 0;
 }
