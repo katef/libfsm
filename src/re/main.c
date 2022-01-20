a5135931f0d38e5cdefbe8c8aca65cbdec08e67c
Scott Vokes <svokes@fastly.com>
2022-01-20 14:23:09 -0500

    re: Add `-C` flag to disable the `# e.g. "blah"` comments.
    `fsm_example`'s current implementation for generating those is very
    expensive, so it can be helpful to disable those comments when using re
    to test generated inputs (where the example match strings would be
    ignored). Disabling `opt.comments` has made a difference of several
    seconds for some of my tests.
    
    This does not update the documentation, but some of the other flags also
    appear to be missing from the XML, so that should probably be done in
    its own pass.

--- src/re/main.c
+++ src/re/main.c
@@ -663,9 +663,10 @@ main(int argc, char *argv[])
 	{
 		int c;
 
-		while (c = getopt(argc, argv, "h" "acwXe:k:" "bi" "sq:r:l:F:" "upMmnfxyz"), c != -1) {
+		while (c = getopt(argc, argv, "h" "aCcwXe:k:" "bi" "sq:r:l:F:" "upMmnfxyz"), c != -1) {
 			switch (c) {
 			case 'a': opt.anonymous_states  = 0;          break;
+			case 'C': opt.comments		= 0;          break;
 			case 'c': opt.consolidate_edges = 0;          break;
 			case 'w': opt.fragment          = 1;          break;
 			case 'X': opt.always_hex        = 1;          break;
