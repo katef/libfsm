15e57ce9e35aeff38c021492dc416f5424a36fff
Damian Gryski <damian@gryski.com>
2023-04-09 11:08:11 -0700

    provide more control over generated function and package names for Go
    Fixes #360
    
    	$ re -r pcre -k pair -e Meow -E 'matchers' -pl amd64_go meow |head -4
    	#include "textflag.h"
    
    	// func MeowMatch(data []byte) int
    	TEXT    ·MeowMatch(SB), NOSPLIT, $0-32
    
    	$ re -r pcre -k pair -e Meow -E 'matchers'-pl go meow |head -4
    	package matchersfsm
    
    	func MeowMatch(data []byte) int {
    		var idx = ^uint(0)

--- src/re/main.c
+++ src/re/main.c
@@ -668,13 +668,14 @@ main(int argc, char *argv[])
 	{
 		int c;
 
-		while (c = getopt(argc, argv, "h" "acwXe:k:" "bi" "sq:r:l:F:" "upMmnftxyz"), c != -1) {
+		while (c = getopt(argc, argv, "h" "acwXe:E:k:" "bi" "sq:r:l:F:" "upMmnftxyz"), c != -1) {
 			switch (c) {
 			case 'a': opt.anonymous_states  = 0;          break;
 			case 'c': opt.consolidate_edges = 0;          break;
 			case 'w': opt.fragment          = 1;          break;
 			case 'X': opt.always_hex        = 1;          break;
 			case 'e': opt.prefix            = optarg;     break;
+			case 'E': opt.package_prefix	= optarg;     break;
 			case 'k': opt.io                = io(optarg); break;
 
 			case 'b': flags |= RE_ANCHORED; break;
