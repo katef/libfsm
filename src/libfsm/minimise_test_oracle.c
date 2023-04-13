17c7cf3f00f24aa0126efeaa76a87f456d1acbf7
Scott Vokes <svokes@fastly.com>
2023-04-13 18:37:48 -0400

    Fix typo.
    This appears in two spots because the test oracle was extracted from the
    property test, but given the implementation size and different boundary
    data structures it's probably not worth factoring it out.

--- src/libfsm/minimise_test_oracle.c
+++ src/libfsm/minimise_test_oracle.c
@@ -90,7 +90,7 @@ fsm_minimise_test_oracle(const struct fsm *fsm)
 	 * the Myhill-Nerode theorem, mark all pairs of states where
 	 * following the same label leads each to a pair of states that
 	 * have already been marked (and are thus transitively
-	 * distinguishable). Repeat until no furhter progress can be
+	 * distinguishable). Repeat until no further progress can be
 	 * made. Any pairs of states still unmarked can be combined into
 	 * a single state, because no possible input leads them to
 	 * observably distinguishable results.
