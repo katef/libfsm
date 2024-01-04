c776e62b2d705c1383a0185451757c8424cfe2c7
Scott Vokes <svokes@fastly.com>
2024-01-04 14:42:42 -0500

    Add include/adt/hash.h `hash_ids` and consolidate uses.
    - Add a function to hash a series of IDs, rather than doing it with
      a loop combining the intermediate hashes in inconsistent ways.
    
    - Add a #define to log a probe count and a limit to assert, and
      rework the hashing code control flow for tracking/asserting on
      the probe count.
    
    - Remove `hash_fnv1a_64` from a logging statement (normally not
      compiled), since that's the only remaining use remove the FNV1a
      functions from hash.h.
    
    - Remove FSM_PHI_32 and FSM_PHI_64, now unused.
    
    - parser.act still uses 32-bit FNV1a, but that's for character-by-
      character hashing, so that still makes sense, and it has its
      own copy of fnv1a.

--- include/adt/hash.h
+++ include/adt/hash.h
@@ -10,10 +10,10 @@
 #include <stdint.h>
 
 #include "adt/common.h"
+#include "fsm/fsm.h"
 
-/* 32 and 64-bit approximations of the golden ratio. */
-#define FSM_PHI_32 0x9e3779b9UL
-#define FSM_PHI_64 (uint64_t)0x9e3779b97f4a7c15UL
+#define HASH_LOG_PROBES 0
+/* #define HASH_PROBE_LIMIT 100 */
 
 SUPPRESS_EXPECTED_UNSIGNED_INTEGER_OVERFLOW()
 static __inline__ uint64_t
@@ -28,38 +28,13 @@ hash_id(uint64_t id)
 	return x * 2685821657736338717LLU;
 }
 
-/* FNV-1a hash function, 32 and 64 bit versions. This is in the public
- * domain. For details, see:
- *
- *     http://www.isthe.com/chongo/tech/comp/fnv/index.html
- */
-
-SUPPRESS_EXPECTED_UNSIGNED_INTEGER_OVERFLOW()
-static __inline__ uint32_t
-hash_fnv1a_32(const uint8_t *buf, size_t length)
-{
-#define FNV1a_32_OFFSET_BASIS 	0x811c9dc5UL
-#define FNV1a_32_PRIME		0x01000193UL
-	uint32_t h = FNV1a_32_OFFSET_BASIS;
-	size_t i;
-	for (i = 0; i < length; i++) {
-		h ^= buf[i];
-		h *= FNV1a_32_PRIME;
-	}
-	return h;
-}
-
 SUPPRESS_EXPECTED_UNSIGNED_INTEGER_OVERFLOW()
 static __inline__ uint64_t
-hash_fnv1a_64(const uint8_t *buf, size_t length)
+hash_ids(size_t count, const fsm_state_t *ids)
 {
-#define FNV1a_64_OFFSET_BASIS   0xcbf29ce484222325UL
-#define FNV1a_64_PRIME		0x100000001b3UL
-	uint64_t h = FNV1a_64_OFFSET_BASIS;
-	size_t i;
-	for (i = 0; i < length; i++) {
-		h ^= buf[i];
-		h *= FNV1a_64_PRIME;
+	uint64_t h = 0;
+	for (size_t i = 0; i < count; i++) {
+		h = hash_id(h ^ ids[i]);
 	}
 	return h;
 }
