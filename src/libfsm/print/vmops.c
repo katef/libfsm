e484294b1ffdbb9310fc30df75162a6ba8a4c51b
Kate F <kate@elide.org>
2024-08-02 15:28:09 +0100

    Support for enids for the vmops output.
    I've handled AMBIG_NONE here, but I haven't distinguished the other ambig modes. Other than AMBIG_NONE, the other modes are all presented as an array of ids, even if it's just a single element. That's because I don't see any reason to give these specialised APIs for the current use-cases for this generated code, which is supposed to be a direct representation of our VM opcodes.

--- src/libfsm/print/vmops.c
+++ src/libfsm/print/vmops.c
@@ -51,6 +51,7 @@ default_accept(FILE *f, const struct fsm_options *opt,
 	void *lang_opaque, void *hook_opaque)
 {
 	const char *prefix;
+	size_t i;
 
 	assert(f != NULL);
 	assert(opt != NULL);
@@ -58,13 +59,21 @@ default_accept(FILE *f, const struct fsm_options *opt,
 
 	(void) hook_opaque;
 
-	prefix = lang_opaque;
+	if (opt->prefix != NULL) {
+		prefix = opt->prefix;
+	} else {
+		prefix = "fsm_";
+	}
 
-	// TODO: print ids
 	(void) ids;
 	(void) count;
 
 	fprintf(f, "%sactionRET, 1", prefix);
+	if (opt->ambig != AMBIG_NONE) {
+		i = * (const size_t *) lang_opaque;
+
+		fprintf(f, ", %zu", i);
+	}
 
 	return 0;
 }
@@ -77,13 +86,21 @@ default_reject(FILE *f, const struct fsm_options *opt,
 
 	assert(f != NULL);
 	assert(opt != NULL);
-	assert(lang_opaque != NULL);
+	assert(lang_opaque == NULL);
 
+	(void) lang_opaque;
 	(void) hook_opaque;
 
-	prefix = lang_opaque;
+	if (opt->prefix != NULL) {
+		prefix = opt->prefix;
+	} else {
+		prefix = "fsm_";
+	}
 
 	fprintf(f, "%sactionRET, 0", prefix);
+	if (opt->ambig != AMBIG_NONE) {
+		fprintf(f, ", 0");
+	}
 
 	return 0;
 }
@@ -121,23 +138,26 @@ static int
 print_end(FILE *f, const struct dfavm_op_ir *op,
 	const struct fsm_options *opt,
 	const struct fsm_hooks *hooks,
-	const char *prefix,
+	const struct ret_list *retlist,
 	enum dfavm_op_end end_bits)
 {
+	size_t i;
+
 	switch (end_bits) {
 	case VM_END_FAIL:
-		if (-1 == print_hook_reject(f, opt, hooks, default_reject,
-			(void *) prefix))
-		{
+		if (-1 == print_hook_reject(f, opt, hooks, default_reject, NULL)) {
 			return -1;
 		}
 		break;
 
 	case VM_END_SUCC:
+		assert(op->ret >= retlist->a);
+
+		i = op->ret - retlist->a;
+
 		if (-1 == print_hook_accept(f, opt, hooks,
 			op->ret->ids, op->ret->count,
-			default_accept,
-			(void *) prefix))
+			default_accept, &i))
 		{
 			return -1;
 		}
@@ -152,9 +172,12 @@ print_end(FILE *f, const struct dfavm_op_ir *op,
 }
 
 static int
-print_branch(FILE *f, const struct dfavm_op_ir *op, const char *prefix)
+print_branch(FILE *f, const struct fsm_options *opt, const struct dfavm_op_ir *op, const char *prefix)
 {
 	fprintf(f, "%sactionGOTO, %" PRIu32, prefix, op->u.br.dest_arg->index);
+	if (opt->ambig != AMBIG_NONE) {
+		fprintf(f, ", 0");
+	}
 
 	return 0;
 }
@@ -176,21 +199,55 @@ print_fetch(FILE *f, const struct fsm_options *opt, const char *prefix)
 	return 0;
 }
 
-/* TODO: eventually to be non-static */
-static int
-fsm_print_vmopsfrag(FILE *f,
+int
+fsm_print_vmops_c(FILE *f,
 	const struct fsm_options *opt,
 	const struct fsm_hooks *hooks,
 	const struct ret_list *retlist,
-	struct dfavm_op_ir *ops,
-	const char *prefix)
+	struct dfavm_op_ir *ops)
 {
-	struct dfavm_op_ir *op;
+	const struct dfavm_op_ir *op;
+	const char *prefix;
 
 	assert(f != NULL);
 	assert(opt != NULL);
+	assert(hooks != NULL);
 
-	(void) retlist;
+	if (opt->fragment) {
+		errno = ENOTSUP;
+		return -1;
+	}
+
+	if (opt->prefix != NULL) {
+		prefix = opt->prefix;
+	} else {
+		prefix = "fsm_";
+	}
+
+	fprintf(f, "#include <stdint.h>\n\n");
+	fprintf(f, "#ifndef %sLIBFSM_VMOPS_H\n", prefix);
+	fprintf(f, "#include \"%svmops.h\"\n", prefix);
+	fprintf(f, "#endif /* %sLIBFSM_VMOPS_H */\n", prefix);
+	fprintf(f, "\n");
+
+	if (opt->ambig != AMBIG_NONE) {
+		fprintf(f, "struct %sret %sRet[] = {\n", prefix, prefix);
+		for (size_t i = 0; i < retlist->count; i++) {
+			fprintf(f, "\t{ (const unsigned []) { ");
+			for (size_t j = 0; j < retlist->a[i].count; j++) {
+				fprintf(f, "%u", retlist->a[i].ids[j]);
+				if (j + 1 < retlist->a[i].count) {
+					fprintf(f, ", ");
+				}
+			}
+			fprintf(f, " }, %zu },\n", retlist->a[i].count);
+		}
+		fprintf(f, "};\n");
+		fprintf(f, "const size_t %sRet_count = sizeof %sRet / sizeof *%sRet;\n", prefix, prefix, prefix);
+		fprintf(f, "\n");
+	}
+
+	fprintf(f, "struct %sop %sOps[] = {\n", prefix, prefix);
 
 	for (op = ops; op != NULL; op = op->next) {
 		if (op->num_incoming > 0) {
@@ -199,14 +256,14 @@ fsm_print_vmopsfrag(FILE *f,
 			}
 		}
 
-		fprintf(f, "\t\t{");
+		fprintf(f, "\t{");
 
 		switch (op->instr) {
 		case VM_OP_STOP:
 			if (-1 == print_cond(f, op, opt, prefix)) {
 				return -1;
 			}
-			if (-1 == print_end(f, op, opt, hooks, prefix, op->u.stop.end_bits)) {
+			if (-1 == print_end(f, op, opt, hooks, retlist, op->u.stop.end_bits)) {
 				return -1;
 			}
 			break;
@@ -215,7 +272,7 @@ fsm_print_vmopsfrag(FILE *f,
 			if (-1 == print_fetch(f, opt, prefix)) {
 				return -1;
 			}
-			if (-1 == print_end(f, op, opt, hooks, prefix, op->u.fetch.end_bits)) {
+			if (-1 == print_end(f, op, opt, hooks, retlist, op->u.fetch.end_bits)) {
 				return -1;
 			}
 			break;
@@ -224,7 +281,7 @@ fsm_print_vmopsfrag(FILE *f,
 			if (-1 == print_cond(f, op, opt, prefix)) {
 				return -1;
 			}
-			if (-1 == print_branch(f, op, prefix)) {
+			if (-1 == print_branch(f, opt, op, prefix)) {
 				return -1;
 			}
 			break;
@@ -237,43 +294,7 @@ fsm_print_vmopsfrag(FILE *f,
 		fprintf(f, "},\n");
 	}
 
-	return 0;
-}
-
-int
-fsm_print_vmops_c(FILE *f,
-	const struct fsm_options *opt,
-	const struct fsm_hooks *hooks,
-	const struct ret_list *retlist,
-	struct dfavm_op_ir *ops)
-{
-	const char *prefix;
-
-	assert(f != NULL);
-	assert(opt != NULL);
-	assert(hooks != NULL);
-
-	if (opt->prefix != NULL) {
-		prefix = opt->prefix;
-	} else {
-		prefix = "fsm_";
-	}
-
-	if (opt->fragment) {
-		if (-1 == fsm_print_vmopsfrag(f, opt, hooks, retlist, ops, prefix)) {
-			return -1;
-		}
-	} else {
-		fprintf(f, "#include <stdint.h>\n\n");
-		fprintf(f, "#ifndef %sLIBFSM_VMOPS_H\n", prefix);
-		fprintf(f, "#include \"%svmops.h\"\n", prefix);
-		fprintf(f, "#endif /* %sLIBFSM_VMOPS_H */\n", prefix);
-		fprintf(f, "struct %sop %sOps[] = {\n", prefix, prefix);
-		if (-1 == fsm_print_vmopsfrag(f, opt, hooks, retlist, ops, prefix)) {
-			return -1;
-		}
-		fprintf(f, "\t};\n");
-	}
+	fprintf(f, "\t};\n");
 
 	return 0;
 }
@@ -294,6 +315,11 @@ fsm_print_vmops_h(FILE *f,
 	(void) retlist;
 	(void) ops;
 
+	if (opt->fragment) {
+		errno = ENOTSUP;
+		return -1;
+	}
+
 	if (opt->prefix != NULL) {
 		prefix = opt->prefix;
 	} else {
@@ -303,11 +329,19 @@ fsm_print_vmops_h(FILE *f,
 	fprintf(f, "#ifndef %sLIBFSM_VMOPS_H\n", prefix);
 	fprintf(f, "#define %sLIBFSM_VMOPS_H\n", prefix);
 	fprintf(f, "#include <stdint.h>\n\n");
+	fprintf(f, "#include <stddef.h>\n\n");
 	fprintf(f, "enum %svmOp { %sopEOF, %sopLT, %sopLE, %sopEQ, %sopNE, %sopGE, %sopGT, %sopALWAYS};\n",
 		prefix, prefix, prefix, prefix, prefix, prefix, prefix, prefix, prefix);
 	fprintf(f, "enum %sactionOp { %sactionRET, %sactionGOTO };\n", prefix, prefix, prefix);
-	fprintf(f, "struct %sop { enum %svmOp op; unsigned char c; enum %sactionOp action; int32_t arg; };\n\n",
+	if (opt->ambig != AMBIG_NONE) {
+		fprintf(f, "struct %sret { const unsigned *ids; size_t count; };\n\n", prefix);
+	}
+	fprintf(f, "struct %sop { enum %svmOp op; unsigned char c; enum %sactionOp action; int32_t arg; int32_t ret; };\n\n",
 		prefix, prefix, prefix);
+	if (opt->ambig != AMBIG_NONE) {
+		fprintf(f, "extern struct %sret %sRet[];\n", prefix, prefix);
+		fprintf(f, "extern const size_t %sRet_count;\n", prefix);
+	}
 	fprintf(f, "#endif /* %sLIBFSM_VMOPS_H */\n", prefix);
 
 	return 0;
@@ -329,6 +363,11 @@ fsm_print_vmops_main(FILE *f,
 	(void) retlist;
 	(void) ops;
 
+	if (opt->fragment) {
+		errno = ENOTSUP;
+		return -1;
+	}
+
 	if (opt->prefix != NULL) {
 		prefix = opt->prefix;
 	} else {
@@ -342,21 +381,32 @@ fsm_print_vmops_main(FILE *f,
 	fprintf(f, "#include \"%svmops.h\"\n", prefix);
 	fprintf(f, "#endif /* %sLIBFSM_VMOPS_H */\n", prefix);
 	fprintf(f, "extern struct %sop %sOps[];\n", prefix, prefix);
+	if (opt->ambig != AMBIG_NONE) {
+		fprintf(f, "extern struct %sret %sRet[];\n", prefix, prefix);
+		fprintf(f, "extern const size_t %sRet_count;\n", prefix);
+	}
 	fprintf(f, "\n");
 
+	fprintf(f, "int %smatch(", prefix);
 	switch (opt->io) {
 	case FSM_IO_PAIR:
-		fprintf(f, "int %smatch(const char *b, const char *e)\n", prefix);
+		fprintf(f, "const char *b, const char *e");
 		break;
 
 	case FSM_IO_STR:
-		fprintf(f, "int %smatch(const char *s)\n", prefix);
+		fprintf(f, "const char *s");
 		break;
 
 	case FSM_IO_GETC:
 		errno = ENOTSUP;
 		return -1;
 	}
+	if (opt->ambig != AMBIG_NONE) {
+		fprintf(f, ",\n");
+		fprintf(f, "\tconst unsigned **ids, size_t *count");
+	}
+	fprintf(f, ")\n");
+
 	fprintf(f, "{\n");
 	fprintf(f, "\tunsigned int i = 0;\n");
 	fprintf(f, "\t/* The compiler doesn't know the op stream will have fetch before the first comparison. */\n");
@@ -421,6 +471,12 @@ fsm_print_vmops_main(FILE *f,
 	fprintf(f, "\t\t}\n");
 	fprintf(f, "\t\tif (ok) {\n");
 	fprintf(f, "\t\t\tif (ops[i].action == %sactionRET) {\n", prefix);
+	if (opt->ambig != AMBIG_NONE) {
+		fprintf(f, "\t\t\t\tif (ops[i].arg) {\n");
+		fprintf(f, "\t\t\t\t\t*ids   = %sRet[ops[i].ret].ids;\n", prefix);
+		fprintf(f, "\t\t\t\t\t*count = %sRet[ops[i].ret].count;\n", prefix);
+		fprintf(f, "\t\t\t\t}\n");
+	}
 	fprintf(f, "\t\t\t\treturn (int) (ops[i].arg);\n");
 	fprintf(f, "\t\t\t}\n");
 	fprintf(f, "\t\t\ti = ops[i].arg;\n");
@@ -435,6 +491,10 @@ fsm_print_vmops_main(FILE *f,
 	fprintf(f, "int main(void)\n");
 	fprintf(f, "{\n");
 	fprintf(f, "\tchar *buf, *p;\n");
+	if (opt->ambig != AMBIG_NONE) {
+		fprintf(f, "\tconst unsigned *ids;\n");
+		fprintf(f, "\tsize_t count, i;\n");
+	}
 	fprintf(f, "\tint r;\n");
 	fprintf(f, "\n");
 	fprintf(f, "\tbuf = malloc(%sBUFFER_SIZE);\n", prefix);
@@ -448,18 +508,34 @@ fsm_print_vmops_main(FILE *f,
 	fprintf(f, "\t\t\tbreak;\n");
 	fprintf(f, "\t\t}\n");
 
+	fprintf(f, "\t\tr = %smatch(", prefix);
 	switch (opt->io) {
 	case FSM_IO_PAIR:
-		fprintf(f, "\t\tr = %smatch(p, p + strlen(p));\n", prefix);
+		fprintf(f, "p, p + strlen(p)");
 		break;
 	case FSM_IO_STR:
-		fprintf(f, "\t\tr = %smatch(p);\n", prefix);
+		fprintf(f, "p");
 		break;
 	case FSM_IO_GETC:
+		// TODO: getc from string buffer
 		errno = ENOTSUP;
 		return -1;
 	}
+	if (opt->ambig != AMBIG_NONE) {
+		fprintf(f, ", &ids, &count");
+	}
+	fprintf(f, ");\n");
+
 	fprintf(f, "\t\tprintf(\"%%smatch\\n\", r == 0 ? \"no \" : \"\");\n");
+	if (opt->ambig != AMBIG_NONE) {
+		fprintf(f, "\t\tif (r) {\n");
+		fprintf(f, "\t\t\tprintf(\"ids:\");\n");
+		fprintf(f, "\t\t\tfor (i = 0; i < count; i++) {\n");
+		fprintf(f, "\t\t\t\tprintf(\" %%u\", ids[i]);\n");
+		fprintf(f, "\t\t\t}\n");
+		fprintf(f, "\t\t\tprintf(\"\\n\");\n");
+		fprintf(f, "\t\t}\n");
+	}
 	fprintf(f, "\t}\n");
 	fprintf(f, "\treturn 0;\n");
 	fprintf(f, "}\n");
